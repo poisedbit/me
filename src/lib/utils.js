2de9c69262661c6da7bcca0c2cf9ac063051b596
Kevin <106220147+poisedbit@users.noreply.github.com>
2024-07-02 08:34:23 -0500

    refactor: move markdown parser from intro page into file parser

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,3 +4,17 @@ export async function read(path) {
     const payload = await res.text();
     return payload;
 }
+
+export function parse(payload, ext) {
+    switch (ext) {
+        case "md":
+            return payload
+                .replace(/^# (.*?$)/gm, "<h1>$1</h1>")
+                .replace(/^## (.*?$)/gm, "<h2>$1</h2>")
+                .replace(/^(?!<).+/gm, `<p>$&</p>`)
+                .replace(/\[(.*?)\]\((.*?)\)/gm, '<a href="$2">$1</a>')
+                .trim();
+        default:
+            return payload;
+    }
+}
--- src/routes/intro.js
+++ src/routes/intro.js
@@ -1,14 +1,7 @@
-import { read } from "/lib/utils.js";
+import { read, parse } from "/lib/utils.js";
 
 const intro = Object.assign(document.createElement("article"), {
-    innerHTML: (text => {
-        return text
-            .replace(/^# (.*?$)/gm, "<h1>$1</h1>")
-            .replace(/^## (.*?$)/gm, "<h2>$1</h2>")
-            .replace(/^(?!<).+/gm, `<p>$&</p>`)
-            .replace(/\[(.*?)\]\((.*?)\)/gm, '<a href="$2">$1</a>')
-            .trim();
-    })(await read("/assets/intro.md")),
+    innerHTML: parse(await read("/assets/intro.md"), "md"),
 });
 
 export default intro;
