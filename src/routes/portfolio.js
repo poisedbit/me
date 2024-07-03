70a7ef927b67bbaf4c9a14098c14364d05019378
Kevin <106220147+poisedbit@users.noreply.github.com>
2024-07-03 13:53:41 -0500

    fix: portfolio repos info not rendering properly

--- src/routes/portfolio.js
+++ src/routes/portfolio.js
@@ -1,60 +1,51 @@
 import { read, parse } from "/lib/utils.js";
 
-const mk_html = async () => {
-    const key = "portfolio";
-    const links = (await read("/assets/repos.txt"))
-        .trim()
-        .split(/[\r\n]/)
-        .reverse();
-    let repos = JSON.parse(localStorage.getItem(key));
+const key = "portfolio";
+const links = (await read("/assets/repos.txt"))
+    .trim()
+    .split(/[\r\n]/)
+    .reverse();
+let repos = JSON.parse(localStorage.getItem(key));
 
-    if (
-        !repos ||
-        links.length !== repos.length ||
-        new Date().getDate() === 30
-    ) {
-        repos = [];
-        for (let i = 0; i < links.length; i++) {
-            const data = parse(await read(links[i]), "json");
-            repos[i] = {
-                name: data.name,
-                desc: data.description,
-                href: data.html_url,
-                last_push: data.pushed_at,
-            };
-        }
-
-        localStorage.setItem(key, JSON.stringify(repos));
+if (!repos || links.length !== repos.length || new Date().getDate() === 30) {
+    repos = [];
+    for (let i = 0; i < links.length; i++) {
+        const data = JSON.parse(await read(links[i]));
+        repos[i] = {
+            name: data.name,
+            desc: data.description,
+            href: data.html_url,
+            last_push: data.pushed_at,
+        };
     }
 
-    return `
-            <table>
-                <thead>
-                    <tr>
-                        <th>name</th>
-                        <th>description</th>
-                        <th>last push</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    ${repos
-                        .map(
-                            val => `    
+    localStorage.setItem(key, JSON.stringify(repos));
+}
+
+const portfolio = Object.assign(document.createElement("article"), {
+    innerHTML: `
+        <table>
+            <thead>
+                <tr>
+                    <th>name</th>
+                    <th>description</th>
+                    <th>last push</th>
+                </tr>
+            </thead>
+            <tbody> ${repos
+                .map(
+                    val => `    
                         <tr>
                             <td><a href="${val.href}" target="_blank">${val.name}</a></td>
                             <td>${val.desc}</td>
                             <td>${val.last_push}</td>
                         </tr>
                     `
-                        )
-                        .join("")}
-                </tbody>
-            </table>
-        `;
-};
-
-const portfolio = Object.assign(document.createElement("article"), {
-    innerHTML: await mk_html(),
+                )
+                .join("")}
+            </tbody>
+        </table>
+    `,
 });
 
 export default portfolio;
