54d04b7730bd1c704fa96228e048415a09d978f1
Kevin <106220147+poisedbit@users.noreply.github.com>
2024-07-08 15:06:31 -0500

    perf: use dynamic imports for routing

--- src/lib/router.js
+++ src/lib/router.js
@@ -1,21 +1,19 @@
-import Err from "/routes/err.js";
-import Root from "/routes/index.js";
-import About from "/routes/about/index.js";
-import Intro from "/routes/intro/index.js";
-import Portfolio from "/routes/portfolio/index.js";
-import Nav from "/view/nav.js";
+const ROUTES = ["/", "/intro", "/portfolio", "/about"];
 
 export default async function init_router(container) {
-    const routes = {
-        "/": Root,
-        "/intro": Intro,
-        "/portfolio": Portfolio,
-        "/about": About,
-    };
-    const path = location.pathname ?? "/";
+    const path = location.pathname;
+
+    if (!ROUTES.includes(path)) {
+        container.append(await use("/routes/err.js"));
+        return;
+    }
 
     container.append(
-        path !== "/" && routes[path] ? Nav() : "",
-        routes[path] ? await routes[path]() : Err()
+        path !== "/" ? await use("/view/nav.js") : "",
+        await use(`/routes${path}/index.js`)
     );
+}
+
+async function use(path) {
+    return (await import(path)).default();
 }
